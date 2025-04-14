c867cfc7943393f2004baa0adbdf5b128f640c58
Mudassir0126 <mudassirahmed13631@gmail.com>
2025-04-14 17:18:37 +0530

    Second Commit

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
+// src/api.js
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'http://localhost:8080/api', // This is your Spring Boot backend
 });
 
 export default API;
